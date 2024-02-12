40f9bdaf64bdf5fa51ec9f6aafb3166125772e52
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-02-13 00:21:42 +0100

    Tested altering a little of the mongodb connection class. Added sample user to main class.
    Locally, the remote connection to mongodb works

--- src/main/java/Honey/Comb/Havoc/Back_Main.java
+++ src/main/java/Honey/Comb/Havoc/Back_Main.java
@@ -1,5 +1,7 @@
 package Honey.Comb.Havoc;
 
+import Honey.Comb.Havoc.database.MongoClientConnection;
+import Honey.Comb.Havoc.entity.UserProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,11 @@ public class Back_Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Back_Main.class, args);
+
+		// Example usage of MongoDBService
+		MongoClientConnection mongoDBService = new MongoClientConnection();
+		UserProfile userProfile = new UserProfile("1", "exampleUser", "user@example.com");
+		mongoDBService.saveUserProfile(userProfile);
 	}
 
 }
--- src/main/java/Honey/Comb/Havoc/database/MongoClientConnection.java
+++ src/main/java/Honey/Comb/Havoc/database/MongoClientConnection.java
@@ -1,6 +1,5 @@
 package Honey.Comb.Havoc.database;
 
-
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
@@ -8,11 +7,19 @@ import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import Honey.Comb.Havoc.entity.UserProfile;
 import org.bson.Document;
 
 public class MongoClientConnection {
-    public static void main(String[] args) {
+
+    private static final String DATABASE_NAME = "YourDatabaseName";
+    private static final String COLLECTION_NAME = "UserDB";
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    public MongoClientConnection() {
         String connectionString = "mongodb+srv://cristofferostberg85:Tomtarna1@cluster0.imetavy.mongodb.net/?retryWrites=true&w=majority";
 
         ServerApi serverApi = ServerApi.builder()
@@ -25,16 +32,21 @@ public class MongoClientConnection {
                 .build();
 
         // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            try {
-                // Send a ping to confirm a successful connection
-                MongoDatabase database = mongoClient.getDatabase("admin");
-                database.runCommand(new Document("ping", 1));
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException e) {
-                e.printStackTrace();
-            }
+        mongoClient = MongoClients.create(settings);
+        database = mongoClient.getDatabase(DATABASE_NAME);
+    }
+
+    public void saveUserProfile(UserProfile userProfile) {
+        try {
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+
+            Document userProfileDocument = new Document("username", userProfile.getUsername())
+                    .append("email", userProfile.getEmail());
+
+            collection.insertOne(userProfileDocument);
+            System.out.println("User profile saved successfully!");
+        } catch (MongoException e) {
+            e.printStackTrace();
         }
     }
 }
-
