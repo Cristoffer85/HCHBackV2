f72ff103a5c558374218a09b01d760cc7d9674f9
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-02-12 22:04:08 +0100

    tested removing local mongo from application.properties

--- src/main/java/com/marcus/awsWebServiceTest/AwsWebServiceTestApplication.java
+++ src/main/java/com/marcus/awsWebServiceTest/AwsWebServiceTestApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HavocBackMain {
+public class AwsWebServiceTestApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(HavocBackMain.class, args);
+		SpringApplication.run(AwsWebServiceTestApplication.class, args);
 	}
+
 }
--- src/main/java/com/marcus/awsWebServiceTest/repository/UserProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marcus.awsWebServiceTest.repository;
-
-import com.marcus.awsWebServiceTest.entity.UserProfile;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
-}
